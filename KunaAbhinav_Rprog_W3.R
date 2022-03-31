0d8301ad0972b56fd2b9ca0efdeb0adb3b97195c
abhinavkuna <83017442+abhinavkuna@users.noreply.github.com>
2022-03-31 11:34:36 +0530

    Add files via upload

---
+++ KunaAbhinav_Rprog_W3.R
@@ -0,0 +1,29 @@
+##R-prog Week 3 by kuna Abhinav
+
+## Make Matrix Fuction
+makeCacheMatrix <- function(x = matrix()) {
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) inv <<- inverse
+  getinverse <- function() inv
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
+}
+
+## CacheInverse Function
+cacheinverse <- function(x, ...) {
+  inv <- x$getinverse()
+  if(!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  matrix_to_invert <- x$get()
+  inv <- solve(matrix_to_invert, ...)
+  x$setinverse(inv)
+  inv
+}
